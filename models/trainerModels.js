7e0bf43e2cf578aca105960ac0b66bc4764ef8e1
naveenboda19 <naveenboda@infinityspark.com>
2020-01-10 14:02:46 +0530

    trainer model

---
+++ models/trainerModels.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const trainerModel = {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    certification: { type: String, default: '' },
+    specialization: { type: String, default: '' },
+    tagline: { type: String, default: '' },    
+    experience: { type: String, default: '' },    
+    id: { type: String, default: '' },    
+    images: [
+        {
+            imgId: { type: String, default: '' },
+            imgVersion: { type: String, default: '' }
+        }
+    ],
+}
+const trainerSchema = mongoose.Schema(trainerModel);
+
+
+module.exports = mongoose.model('Trainer', trainerSchema);
